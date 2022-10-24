5a5675ce6d77f4495adfda48cddcc0bd3729e488
Dennis ten Hoove <dennistenhoove@protonmail.com>
2022-10-25 01:37:36 +0200

    Use Syslinux and Systemd-boot as bootloaders

--- profiledef.sh
+++ profiledef.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
+iso_name="arkanelinux"
 iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_publisher="Arkane Linux <https://arkanelinux.org>"
+iso_application="Arkane Linux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
-install_dir="arch"
+install_dir="arkane"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
-           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+bootmodes=(
+	'bios.syslinux.mbr'
+	'bios.syslinux.eltorito'
+	'uefi-x64.systemd-boot.esp'
+	'uefi-x64.systemd-boot.eltorito'
+)
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
@@ -22,4 +25,6 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/etc/gshadow"]="0:0:400"
+  ["/etc/sudoers"]="0:0:400"
 )
