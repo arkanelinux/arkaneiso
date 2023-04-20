a45a37b4941b250a63fb0a53f5c01597a411c477
Dennis ten Hoove <dennistenhoove@protonmail.com>
2023-04-20 23:39:02 +0200

    Start with clean gnome iso

--- gnome/profiledef.sh
+++ gnome/profiledef.sh
@@ -18,13 +18,7 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '15' '-b' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
-  ["/root"]="0:0:750"
-  ["/root/.automated_script.sh"]="0:0:755"
-  ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
-  ["/usr/local/bin/generate_locale"]="0:0:755"
   ["/etc/gshadow"]="0:0:400"
   ["/etc/sudoers"]="0:0:400"
-  ["/etc/locale.gen"]="0:0:644"
+  ["/root"]="0:0:750"
 )
