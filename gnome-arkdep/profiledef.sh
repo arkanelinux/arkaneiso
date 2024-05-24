09ad2cd8817ac5e2dfcfd3b1b69a36f9c8ab6f28
Dennis ten Hoove <dennistenhoove@protonmail.com>
2024-05-24 04:16:39 +0200

    Support 32-bit UEFI, increase compress level

--- gnome-arkdep/profiledef.sh
+++ gnome-arkdep/profiledef.sh
@@ -8,14 +8,12 @@ iso_application="Arkane Linux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arkane"
 buildmodes=('iso')
-bootmodes=(
-	'uefi-x64.systemd-boot.esp'
-	'uefi-x64.systemd-boot.eltorito'
-)
+bootmodes=('uefi-ia32.systemd-boot.esp' 'uefi-x64.systemd-boot.esp'
+           'uefi-ia32.systemd-boot.eltorito' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '15' '-b' '1M')
+airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '19' '-b' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/etc/gshadow"]="0:0:400"
